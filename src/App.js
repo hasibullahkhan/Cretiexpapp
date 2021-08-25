a803b8a924c37ea11d78222599378ee8c54ebe4d
Hasibullah khan <Hasibpak@gmail.com>
2021-08-26 00:30:51 +0400

    change in app.js add moudle router-dom create Menu.js,Menustyle.css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import React from 'react'
 import CertificateEntry from "./Component/CardCertificate/Certificate"
 import ItemMasterEntry from "./Component/CardCertificate/ItemMaster"
+import Menu from "./Component/Menu"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div  >
-
-      <CertificateEntry head1="Certificate Entry" lbl1="Certificate# " lbl2="Vender#" lbl3="Item#" lbl4="Amount" lbl5="Total Notification" lbl6="No of Notification Send" />
-      {<br />}
-      {<br />}
-      <ItemMasterEntry head1="Item Master Entry" lbl1="Item# " lbl2="Name#" lbl3="Description#" />
-      <div>
-        {<br />}
-       <ItemMasterEntry head1="Vender Master Entry" lbl1="Vender# " lbl2="Name#" lbl3="Description#" /> 
+    <Router>
+      <div  >
+        <Menu />
+        <Switch>
+          <Route path="/Certificate" exact component={Certificate} />
+          <Route path="/ItemMaster" component={ItemMaster} />
+          <Route path="/VenderMaster" component={VenderMaster} />
+        </Switch>
       </div>
-    </div>
+    </Router>
   )
 }
 
 export default App
+
+function Certificate() {
+  return (
+    <CertificateEntry head1="Certificate Entry" lbl1="Certificate# " lbl2="Vender#" lbl3="Item#" lbl4="Amount" lbl5="Total Notification" lbl6="No of Notification Send" />
+  );
+}
+
+function ItemMaster() {
+  return (
+    <ItemMasterEntry head1="Item Master Entry" lbl1="Item# " lbl2="Name#" lbl3="Description#" />
+  )
+}
+
+function VenderMaster() {
+  return (
+    <ItemMasterEntry head1="Vender Master Entry" lbl1="Vender# " lbl2="Name#" lbl3="Description#" />
+  )
+}
+
+
 
--- src/Component/Menu.js
+++ src/Component/Menu.js
@@ -1,55 +1,18 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import './Menustyle.css'
+import { Link } from "react-router-dom";
 
-export default function App() {
+const Menu = () => {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+    <div className="Menustyle">
+      <ul>
+        <li> <Link to="Certificate">Certificate</Link> </li>
+        <li><Link to="ItemMaster">Item Master</Link></li>
+        <li><Link to="VenderMaster">Vender Master</Link></li>
+        <li><Link to="ItemMaster">user Alocation</Link></li>
+      </ul>
+    </div>
+  )
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
+export default Menu;
